61dad1d53b7b138102be9f188e658b0072676d75
Ola Sobhy <75180179+olasobhy@users.noreply.github.com>
2023-07-11 02:34:44 +0300

    Delete Task-6-AI directory

--- Task-6-AI/Problem1_T6.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import numpy as np
-print(np.eye(3)*9)
-
--- Task-6-AI/Problem3_T6.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
-import numpy as np
-print(np.zeros((3,3) , dtype=int))
-
